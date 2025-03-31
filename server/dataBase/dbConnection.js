f0fd83762ea410d021beb48ec2a9d8adffe14f8c
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-03-31 14:56:09 +0530

    hello

--- server/dataBase/dbConnection.js
+++ server/dataBase/dbConnection.js
@@ -1,8 +1,9 @@
 const MonGoose = require("mongoose")
-
+require("dotenv").config()
 
 exports.DbConnect = async () => {
-    MonGoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB Connected"))
+    console.log(process.env.MONGO_URI)
+    await MonGoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB Connected"))
         .catch((err) => {
             console.log(err)
             console.log("error")
